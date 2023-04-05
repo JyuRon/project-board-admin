a5a4003fe13db19ca704ce4b9e9894589cea0558
Jyuka <yoyksks3@gmail.com>
2023-04-05 18:33:45 +0900

    게시판 서비스의 호스트명을 프로퍼티로 등록
    - 이 정보를 통해 게시판 서비스의 api에 접근한다.
    - 이렇게 분리해서 추후 도메인 호스트 정보를 좀 더 유연하게 관리할 수 있게끔 함.
    - 해당 프로퍼티에 접근할 수 있는 레코드 클래스도 추가.

---
+++ src/main/java/com/example/projectboardadmin/dto/properties/ProjectProperties.java
@@ -0,0 +1,19 @@
+package com.example.projectboardadmin.dto.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 어드민 프로젝트 전용 프토퍼티
+ *
+ * @param board
+ */
+@ConfigurationProperties("project")
+public record ProjectProperties(Board board) {
+
+    /**
+     * 게시판 관련 프로퍼티
+     *
+     * @param url 게시판 서비스 호스트 명
+     */
+    public record Board(String url) {}
+}
