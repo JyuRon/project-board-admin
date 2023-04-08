9a970b5fd41cb671649f9efbf0f658739b2a0068
Jyuka <yoyksks3@gmail.com>
2023-04-08 13:47:40 +0900

    회원 관리 기능 컨트롤러, 서비스, 뷰 구현

--- src/main/java/com/example/projectboardadmin/service/UserAccountManagementService.java
+++ src/main/java/com/example/projectboardadmin/service/UserAccountManagementService.java
@@ -1,25 +1,56 @@
 package com.example.projectboardadmin.service;
 
 import com.example.projectboardadmin.dto.UserAccountDto;
+import com.example.projectboardadmin.dto.properties.ProjectProperties;
+import com.example.projectboardadmin.dto.response.UserAccountClientResponse;
+import com.example.projectboardadmin.dto.response.UserAccountResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserAccountManagementService {
 
+    private final RestTemplate restTemplate;
+    private final ProjectProperties projectProperties;
+
     public List<UserAccountDto> getUserAccounts() {
-        return List.of();
+        URI uri = UriComponentsBuilder.fromHttpUrl(projectProperties.board().url() + "/api/userAccounts")
+                .queryParam("size",10000)
+                .build()
+                .toUri();
+
+        UserAccountClientResponse response = restTemplate.getForObject(uri, UserAccountClientResponse.class);
+
+        return Optional.ofNullable(response)
+                .orElseGet(UserAccountClientResponse::empty)
+                .userAccounts();
     }
 
     public UserAccountDto getUserAccount(String userId) {
-        return null;
+        URI uri = UriComponentsBuilder.fromHttpUrl(projectProperties.board().url() + "/api/userAccounts/" + userId)
+                .build()
+                .toUri();
+
+        UserAccountDto response = restTemplate.getForObject(uri, UserAccountDto.class);
+
+        return Optional.ofNullable(response)
+                .orElseThrow(() -> new NoSuchElementException("사용자가 없습니다 - articleCommentId: " + userId));
     }
 
     public void deleteUserAccount(String userId) {
+        URI uri = UriComponentsBuilder.fromHttpUrl(projectProperties.board().url() + "/api/userAccounts/" + userId)
+                .build()
+                .toUri();
 
+        restTemplate.delete(uri);
     }
 
 }
